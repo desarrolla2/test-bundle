323f416137b0e09cd043dcab39166a47ab7eb18f
Daniel González <daniel@devtia.com>
2018-02-22 12:29:31 +0100

    [] - cs

--- src/Model/Key.php
+++ src/Model/Key.php
@@ -1,10 +1,14 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: daniel
- * Date: 22/02/18
- * Time: 12:19
+/*
+ * This file is part of the desarrolla2 test bundle package
+ *
+ * Copyright (c) 2017-2018 Devtia Soluciones
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Daniel González <daniel@devtia.com>
  */
 
 namespace Desarrolla2\TestBundle\Model;
