b63631690c258fa90b1fc8e655fa7db93f08c630
Daniel González <daniel@devtia.com>
2018-05-23 10:47:28 +0200

    fix: Auto-registration of the command .. is deprecated

---
+++ src/DependencyInjection/TestExtension.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * This file is part of the desarrolla2 test bundle package
+ *
+ * Copyright (c) 2017-2018 Devtia Soluciones
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Daniel González <daniel@devtia.com>
+ */
+
+namespace Desarrolla2\TestBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * This is the class that loads and manages your bundle configuration.
+ *
+ * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
+ */
+class TestExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
